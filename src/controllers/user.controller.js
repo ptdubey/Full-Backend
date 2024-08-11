27588fcf9ebb1f27a7b8bd99553dfb0a57ad53e8
ptdubey <adityababa@gmail.com>
2024-08-11 10:51:48 +0530

    Register USer SucessFully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,21 +15,30 @@ const registerUser = asyncHandler(async (req, res) => {
   //return res
 
   const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+  // console.log("email", email);
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All Field Are Required");
   }
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUser) {
     throw new ApiError(409, "User Already Exist");
   }
+  console.log(req.files);
 
-  const avatarLocalPath = req.files?.avatar[0]?.avatarLocalPath;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
